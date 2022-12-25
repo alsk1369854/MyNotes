8575eaf173b3cadf91d41e1e60d4f7cfac5fc344
alsk1369854 <alsk1369854@gmail.com>
2022-12-26 03:34:17 +0800

    update

---
+++ Learn_app/Babel_TypeScripte/lib/TestModule.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = function _default() {
+  return 'I\'m Test Module';
+};
+exports.default = _default;
+//# sourceMappingURL=TestModule.js.map
---
+++ Learn_app/Babel_TypeScripte/src/index.ts
@@ -0,0 +1,20 @@
+import hi from './TestModule'
+
+export const x: number = 32;
+
+//@ts-ignore
+// ignoreFun();
+
+
+console.log(document.getElementById('a'))
+
+console.log(hi());
+console.log(x);
+
+console.log(1231);
+
+console.log(1231);
+console.log(1231);
+
+
+export const a = (()=> hi)();
