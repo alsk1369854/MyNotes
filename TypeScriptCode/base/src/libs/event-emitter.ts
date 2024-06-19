0644302fad7dfa9a23a6e045433e1e6b0ba76dc2
ChiaMing <alsk1369854@gmail.com>
2024-06-19 17:50:19 +0800

    feat: update ts code

--- TypeScriptCode/base/src/libs/event-emitter.ts
+++ TypeScriptCode/base/src/libs/event-emitter.ts
@@ -1,26 +1,45 @@
-const eventNames = ['BPMN:DOWNLOAD_DIAGRAM'] as const;
-type EventName = (typeof eventNames)[number];
+// const eventNames = ["BPMN:DOWNLOAD_DIAGRAM"] as const;
+// type EventName = (typeof eventNames)[number];
 
-class EventEmitter {
-  private listeners: Record<EventName, Set<Function>> = eventNames.reduce(
-    (listeners, eventName) => {
-      listeners[eventName] = new Set();
-      return listeners;
-    },
-    {} as Record<EventName, Set<Function>>
-  );
+type DefaultEventMap = {
+  [_key in string]: any;
+};
 
-  public on(eventName: EventName, listener: Function): void {
-    this.listeners[eventName].add(listener);
+type Listener<
+  EventMap extends DefaultEventMap,
+  EventName extends keyof EventMap
+> = (event: EventMap[EventName]) => void;
+
+export class EventEmitter<EventMap extends DefaultEventMap = DefaultEventMap> {
+  private listenerCollections: Map<keyof EventMap, Set<Function>> = new Map();
+
+  public on<EventName extends keyof EventMap>(
+    eventName: EventName,
+    listener: Listener<EventMap, EventName>
+  ): void {
+    this.getListenerCollection(eventName).add(listener);
   }
 
-  public off(eventName: EventName, listener: Function): void {
-    this.listeners[eventName].delete(listener);
+  public off<EventName extends keyof EventMap>(
+    eventName: EventName,
+    listener: Listener<EventMap, EventName>
+  ): void {
+    this.getListenerCollection(eventName).delete(listener);
   }
 
-  public emit(eventName: EventName, ...args: any[]): void {
-    this.listeners[eventName].forEach((listener) => listener(...args));
+  public emit<EventName extends keyof EventMap>(
+    eventName: EventName,
+    event: EventMap[EventName]
+  ): void {
+    this.getListenerCollection(eventName).forEach((listener) =>
+      listener(event)
+    );
+  }
+
+  private getListenerCollection(eventName: keyof EventMap): Set<Function> {
+    return (
+      this.listenerCollections.get(eventName) ??
+      this.listenerCollections.set(eventName, new Set()).get(eventName)!
+    );
   }
 }
-
-export const eventEmitter = new EventEmitter();
