a3897b07b926a0ba8649e17e6568f5b8cf28f6ef
ChiaMing <alsk1369854@gmail.com>
2024-08-02 16:33:38 +0800

    update python code

--- PythonCode/FastAPI/src/routers/users.py
+++ PythonCode/FastAPI/src/routers/users.py
@@ -1,5 +1,5 @@
-from typing import Dict
-from fastapi import APIRouter, HTTPException, Path
+from typing import Dict, Optional
+from fastapi import APIRouter, HTTPException, Path, Query, Body, status
 from pydantic import BaseModel
 from ..utils import uuidutils
 
@@ -10,6 +10,9 @@ router = APIRouter(
     tags=['users']
 )
 
+class Message(BaseModel):
+    message: str
+
 class User(BaseModel):
     id: str
     name: str
@@ -19,7 +22,17 @@ class CreateUserRequestBody(BaseModel):
     name: str
     age: int
 
-users: Dict[str, User] = {}
+class UpdateUserRequestBody(BaseModel):
+    name: Optional[str]
+    age: Optional[int]
+
+users: Dict[str, User] = {
+    "1" : User(**{
+        "id": "1",
+        "name": "string",
+        "age": 0
+    })
+}
 
 # Create
 @router.post(
@@ -37,10 +50,39 @@ async def create_user(body: CreateUserRequestBody):
     response_model=User
 )
 async def get_user_by_id(
-    user_id: str
-    # user_id: str = Path(None, title="使用者ID title", description="使用者ID description")
+    user_id: str = Path(..., description="使用者ID description")
+) -> User:
+    if user_id not in users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    return users[user_id]
+
+# Get item
+@router.get(
+    '/labe-value/{user_id}',
+    response_model=Message 
+)
+async def get_user_value(
+    user_id: str = Path(..., description="User ID"),
+    label: str = Query(..., title="Label title", description="User label name")
+    # label: Optional[str] = Query(None, title="Label title", description="User label name")
 ):
     if user_id not in users:
-        raise HTTPException(status_code=404, detail="user not found")
-    return users[user_id]
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    user: User = users[user_id]
+
+    if label not in user.dict():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user label not found")
+
+    return Message(message=str(user.dict()[label]))
+
+# Put
+@router.put(
+    '/{user_id}',
+    response_model=User
+)
+async def update_user_by_id(
+    user_id: str = Path(..., description="使用者ID"),
+    body: UpdateUserRequestBody = Body(..., description="請求體")
+):
+    print(body)
 
